c283bc9603125ec735c8ed4ac5e3ae41b2197603
sejinik <sejinik@naver.com>
2020-02-24 16:38:41 +0900

    Change Servlet mapping [server.xml -> @]

--- pro05/src/sec01/ex01/FirstServlet.java
+++ pro05/src/sec01/ex01/FirstServlet.java
@@ -3,11 +3,13 @@ package sec01.ex01;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.sql.rowset.serial.SerialException;
 
+@WebServlet("/first")
 public class FirstServlet extends HttpServlet{
 	
 	@Override
