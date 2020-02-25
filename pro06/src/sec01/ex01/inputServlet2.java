ea5a410f01a602d6796849f7da74c1cc4878090e
sejinik <sejinik@naver.com>
2020-02-25 14:28:46 +0900

    using getParameterNames

---
+++ pro06/src/sec01/ex01/inputServlet2.java
@@ -0,0 +1,39 @@
+package sec01.ex01;
+
+import java.io.IOException;
+import java.util.Enumeration;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/input2")
+public class inputServlet2 extends HttpServlet {
+
+
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("inputServlet2 init");
+	}
+
+	
+	public void destroy() {
+		System.out.println("destroy init");
+	}
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Enumeration enu = request.getParameterNames();
+		while(enu.hasMoreElements()) {
+			String name = (String) enu.nextElement();
+			String[] values = request.getParameterValues(name);
+			for(String value: values) {
+				System.out.println("name = " +name +", value = " +value);
+			}
+		}
+	}
+
+}
