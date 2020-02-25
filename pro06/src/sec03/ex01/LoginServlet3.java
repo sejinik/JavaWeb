f2e28f7d882fcd4601b6c880b4b6743bc955ee9a
sejinik <sejinik@naver.com>
2020-02-25 23:06:43 +0900

    using post method in login.html

---
+++ pro06/src/sec03/ex01/LoginServlet3.java
@@ -0,0 +1,32 @@
+package sec03.ex01;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/login3")
+public class LoginServlet3 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("login3 init");
+	}
+
+	public void destroy() {
+		System.out.println("login3 destroy");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		String id = request.getParameter("user_id");
+		String pw = request.getParameter("user_pw");
+		System.out.println("아이디 : "+id);
+		System.out.println("비밀번호 : "+pw);
+	}
+
+}
