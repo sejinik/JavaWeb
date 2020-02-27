c33d86c91a0db541f585015a466cb9dacea0fca1
sejinik <sejinik@naver.com>
2020-02-28 01:49:44 +0900

    location을 통한 포워딩

--- pro08/src/sec01/ex03/SecondServlet.java
+++ pro08/src/sec01/ex03/SecondServlet.java
@@ -1,17 +1,25 @@
-package sec01.ex02;
+package sec01.ex03;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-//@WebServlet("/first")
-public class FirstServlet extends HttpServlet {
+@WebServlet("/second")
+public class SecondServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.addHeader("Refresh","1;url=second");
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		
+		out.print("<html><body>");
+		out.print("location를 이용한 redirect 실습입니다");
+		out.print("</body></html>");
 	}
+
 
 }
