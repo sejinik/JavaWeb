58ec5e3a659b548b53522bd4bccee32ca559f36b
sejinik <sejinik@naver.com>
2020-03-11 20:09:43 +0900

    스크립트 요소를 이용한 회원정보 출력

---
+++ pro12/src/sec02/ex01/MemberVO.java
@@ -0,0 +1,43 @@
+package sec02.ex01;
+
+import java.sql.Date;
+
+public class MemberVO {
+	private String id;
+	private String pwd;
+	private String name;
+	private String eamil;
+	private Date joinDate;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEamil() {
+		return eamil;
+	}
+	public void setEamil(String eamil) {
+		this.eamil = eamil;
+	}
+	public Date getJoinDate() {
+		return joinDate;
+	}
+	public void setJoinDate(Date joinDate) {
+		this.joinDate = joinDate;
+	}
+	
+	
+}
