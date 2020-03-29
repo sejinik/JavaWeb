948f64fed70390490282e36436cedc369256c250
sejinik <sejinik@naver.com>
2020-03-30 00:07:45 +0900

    Added BoardController.java

--- pro17/src/sec03/brd04/BoardService.java
+++ pro17/src/sec03/brd04/BoardService.java
@@ -1,5 +1,26 @@
 package sec03.brd04;
 
-public class BoardService {
+import java.util.List;
 
+public class BoardService {
+	BoardDAO boardDAO;
+	
+	public BoardService() {
+		boardDAO = new BoardDAO();
+	}
+	
+	public List<ArticleVO> listArticles(){
+		List<ArticleVO> articlesList = boardDAO.selectAllArticles();
+		return articlesList;
+	}
+	
+	public int addArticle(ArticleVO article) {
+		return boardDAO.insertNewArticle(article);
+	}
+	
+	public ArticleVO viewArticle(int articleNO) {
+		ArticleVO article = null;
+		article = boardDAO.selectArticle(articleNO);
+		return article;
+	}
 }
