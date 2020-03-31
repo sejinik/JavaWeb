6716eae5707bed18767d64037ac780503f61d00e
sejinik <sejinik@naver.com>
2020-04-01 01:41:22 +0900

    글 삭제 기능 구현

--- pro17/src/sec03/brd06/BoardController.java
+++ pro17/src/sec03/brd06/BoardController.java
@@ -1,4 +1,4 @@
-package sec03.brd05;
+package sec03.brd06;
 
 import java.io.File;
 import java.io.IOException;
@@ -22,7 +22,7 @@ import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.apache.commons.io.FileUtils;
 
 
-//@WebServlet("/board/*")
+@WebServlet("/board/*")
 public class BoardController extends HttpServlet {
 	private static String ARTICLE_IMAGE_REPO = "C:\\board\\article_image";
 	BoardService boardService;
@@ -61,17 +61,17 @@ public class BoardController extends HttpServlet {
 				
 				articlesList = boardService.listArticles();
 				request.setAttribute("articlesList", articlesList);
-				nextPage = "/board04/listArticles.jsp";
+				nextPage = "/board05/listArticles.jsp";
 				
 			} else if(action.equals("/listArticles.do")) {
 				
 				articlesList = boardService.listArticles();
 				request.setAttribute("articlesList", articlesList);
-				nextPage = "/board04/listArticles.jsp";
+				nextPage = "/board05/listArticles.jsp";
 				
 			} else if(action.equals("/articleForm.do")) {
 				
-				nextPage = "/board04/articleForm.jsp";
+				nextPage = "/board05/articleForm.jsp";
 				
 			} else if(action.equals("/addArticle.do")) {
 				
@@ -109,7 +109,7 @@ public class BoardController extends HttpServlet {
 				articleVO = boardService.viewArticle(Integer.parseInt(articleNO));
 				request.setAttribute("article", articleVO);
 				
-				nextPage = "/board04/viewArticle.jsp";
+				nextPage = "/board05/viewArticle.jsp";
 				
 			} else if(action.equals("/modArticle.do")) {
 				
@@ -145,6 +145,22 @@ public class BoardController extends HttpServlet {
 						+ request.getContextPath()+"/board/viewArticle.do?"
 								+ "articleNO="+articleNO+"';</script>");
 				return;
+				
+			} else if(action.equals("/removeArticle.do")) {
+					
+				int articleNO = Integer.parseInt(request.getParameter("articleNO"));
+				List<Integer> articleNOList = boardService.removeArticle(articleNO);
+				for(int _articleNO : articleNOList) {
+					File imgDir = new File(ARTICLE_IMAGE_REPO+"\\"+_articleNO);
+					if(imgDir.exists()) {
+						FileUtils.deleteDirectory(imgDir);
+					}
+				}
+				PrintWriter pw = response.getWriter();
+				pw.print("<script> alert('글을 삭제했습니다'); "
+						+ " location.href='"+request.getContextPath()+"/board/listArticles.do';</script>");
+				return;
+				
 			}
 			
 			RequestDispatcher dispatch = request.getRequestDispatcher(nextPage);
