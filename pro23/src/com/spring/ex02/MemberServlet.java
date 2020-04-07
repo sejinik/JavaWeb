3203a2a2102a382c51be27c6eeecc3e7399b4eb2
sejinik <sejinik@naver.com>
2020-04-07 22:21:28 +0900

    mybatis를 이용한 회원 검색 by id,pw

--- pro23/src/com/spring/ex02/MemberServlet.java
+++ pro23/src/com/spring/ex02/MemberServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet("/mem2.do")
+//@WebServlet("/mem2.do")
 public class MemberServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
