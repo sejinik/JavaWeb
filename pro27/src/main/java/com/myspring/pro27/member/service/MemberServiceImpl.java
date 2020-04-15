5ce659ae17791ca24dc120e901da65ac629d5e48
sejinik <sejinik@naver.com>
2020-04-15 23:16:34 +0900

    Maven + STS + Spring을 이용한 회원 정보 조회, 등록, 삭제 구현

---
+++ pro27/src/main/java/com/myspring/pro27/member/service/MemberServiceImpl.java
@@ -0,0 +1,36 @@
+package com.myspring.pro27.member.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Service;
+
+import com.myspring.pro27.member.dao.MemberDAO;
+import com.myspring.pro27.member.vo.MemberVO;
+
+@Service("memberService")
+public class MemberServiceImpl implements MemberService {
+
+	@Autowired
+	private MemberDAO memberDAO;
+	
+	@Override
+	public List<MemberVO> listMembers() throws DataAccessException {
+		List<MemberVO> membersList = memberDAO.selectAllMemberList();
+		return membersList;
+	}
+
+	@Override
+	public int addMember(MemberVO memberVO) throws DataAccessException {
+		int result = memberDAO.insertMember(memberVO);
+		return result;
+	}
+
+	@Override
+	public int removeMember(String id) throws DataAccessException {
+		int result = memberDAO.deleteMember(id);
+		return result;
+	}
+
+}
