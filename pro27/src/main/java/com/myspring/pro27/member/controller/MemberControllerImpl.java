5f67d5f666b5b999cdcbfd2e914cb2bd5454ba3e
sejinik <sejinik@naver.com>
2020-04-18 01:40:30 +0900

    Interceptor를 이용한 viewName 설정

--- pro27/src/main/java/com/myspring/pro27/member/controller/MemberControllerImpl.java
+++ pro27/src/main/java/com/myspring/pro27/member/controller/MemberControllerImpl.java
@@ -37,8 +37,8 @@ public class MemberControllerImpl extends MultiActionController implements Membe
 	@RequestMapping(value="/member/listMembers.do", method=RequestMethod.GET)
 	public ModelAndView listMembers(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		request.setCharacterEncoding("utf-8");
-		String viewName = getViewName(request);
-		
+		//String viewName = getViewName(request);
+		String viewName = (String) request.getAttribute("viewName");
 		logger.info("viewName : " +viewName);
 		logger.debug("viewName : "+viewName);
 		
@@ -71,7 +71,9 @@ public class MemberControllerImpl extends MultiActionController implements Membe
 
 	@RequestMapping(value="/member/*Form.do",method=RequestMethod.GET)
 	public ModelAndView form(@RequestParam(value="result", required=false) String result, HttpServletRequest request, HttpServletResponse response) throws Exception {
-		String viewName = getViewName(request);
+		//String viewName = getViewName(request);
+		String viewName = (String) request.getAttribute("viewName");
+
 		ModelAndView mav = new ModelAndView(viewName);
 		mav.addObject("result", result);
 		return mav;
@@ -106,36 +108,6 @@ public class MemberControllerImpl extends MultiActionController implements Membe
 		return mav;
 	}
 	
-	private String getViewName(HttpServletRequest request) throws Exception {
-		String contextPath = request.getContextPath();
-		String uri = (String) request.getAttribute("javax.servlet.include.request_uri");
-		if (uri == null || uri.trim().equals("")) {
-			uri = request.getRequestURI();
-		}
-
-		int begin = 0;
-		if (!((contextPath == null) || ("".equals(contextPath)))) {
-			begin = contextPath.length();
-		}
-
-		int end;
-		if (uri.indexOf(";") != -1) {
-			end = uri.indexOf(";");
-		} else if (uri.indexOf("?") != -1) {
-			end = uri.indexOf("?");
-		} else {
-			end = uri.length();
-		}
-
-		String viewName = uri.substring(begin, end);
-		if (viewName.indexOf(".") != -1) {
-			viewName = viewName.substring(0, viewName.lastIndexOf("."));
-		}
-		if (viewName.lastIndexOf("/") != -1) {
-			viewName = viewName.substring(viewName.lastIndexOf("/", 1), viewName.length());
-		}
-		return viewName;
-	}
 
 	
 }
