764eb5bd903bebe718db627b34634d07b6179fe9
sejinik <sejinik@naver.com>
2020-04-19 02:20:54 +0900

    RestController를 통한 Restful 실습

---
+++ pro29/src/main/java/com/myspring/pro29/ex01/TestController.java
@@ -0,0 +1,66 @@
+package com.myspring.pro29.ex01;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+//Rest 방식의 데이터 처리 지원
+@RestController
+@RequestMapping("/test/*")
+public class TestController {
+
+	@RequestMapping("/hello")
+	public String hello() {
+		return "Hello REST!!";
+	}
+	
+	@RequestMapping("/member")
+	public MemberVO member() {
+		MemberVO vo = new MemberVO();
+		vo.setId("sejinik");
+		vo.setPwd("123");
+		vo.setName("sejin");
+		vo.setEmail("sejinik@gmail.com");
+		return vo;
+	}
+	
+	@RequestMapping("/membersList")
+	public List<MemberVO> listMembers(){
+		List<MemberVO> membersList = new ArrayList<MemberVO>();
+		
+		for(int i=0;i<10;i++) {
+			MemberVO member = new MemberVO();
+			member.setId("sejin"+i);
+			member.setName("name"+i);
+			member.setPwd("pwd"+i);
+			member.setEmail("mail"+i);
+			membersList.add(member);
+		}
+		return membersList;
+	}
+	
+	@RequestMapping("/membersMap")
+	public Map<Integer, MemberVO> membersMap(){
+		Map<Integer, MemberVO> membersMap = new HashMap<Integer, MemberVO>();
+		for(int i=0;i<5;i++) {
+			MemberVO member = new MemberVO();
+			member.setId("id"+i);
+			member.setPwd("pwd"+i);
+			member.setName("name"+i);
+			member.setEmail("email"+i);
+			membersMap.put(i, member);
+		}
+		return membersMap;
+	}
+	
+	@RequestMapping(value="/notice/{num}",method=RequestMethod.GET)
+	public int notice(@PathVariable("num") int num) throws Exception {
+		return num;
+	}
+}
